72a58fcd9772b810073aaf6293b47c8ce9353676
Robert Elliot <rob@lidalia.org.uk>
2015-06-17 20:27:49 +0100

    Single convenient client

--- src/main/scala/HigherKindedTypes.scala
+++ src/main/scala/HigherKindedTypes.scala
@@ -11,28 +11,34 @@ class Request[T](uri: String, method: Method, entityType: Class[T])
 
 class Response[T](val status: Int, val value: T)
 
-class Client1 {
-  def execute[T](request: Request[T]): Response[T] = ???
+trait BaseClient[Result[_]] {
+  def execute[T](request: Request[T]): Result[T]
 }
 
-class Client2(delegate: Client1) {
+class Client1 extends BaseClient[Response] {
+  override def execute[T](request: Request[T]): Response[T] = ???
+}
+
+object Client2 {
+  type Is[T] = T
+}
+
+class Client2(delegate: Client1) extends BaseClient[Client2.Is] {
   def execute[T](request: Request[T]): T = delegate.execute(request).value
 }
 
-class ConvenientClient1(delegate: Client1) {
-  def get[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.GET, classOf[Json]))
-  def delete[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.DELETE, classOf[Json]))
-}
-
-class ConvenientClient2(delegate: Client2) {
+class ConvenientClient[Result[_]](delegate: BaseClient[Result]) {
   def get[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.GET, classOf[Json]))
   def delete[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.DELETE, classOf[Json]))
 }
 
 object Main extends App {
-  val client = new Client2(new Client1)
-  val response: Json = client.execute(new Request("http://example.com", Method.GET, classOf[Json]))
 
-  val convenientClient2 = new ConvenientClient2(new Client2(new Client1))
-  val response2: Json = convenientClient2.get("http://example.com", classOf[Json])
+  val convenientClient1 = new ConvenientClient(new Client1)
+  val response1: Response[Json] = convenientClient1.get("http://example.com", classOf[Json])
+
+  val convenientClient2 = new ConvenientClient(new Client2(new Client1))
+  val response: Json = convenientClient2.get("http://example.com", classOf[Json])
+
+
 }
