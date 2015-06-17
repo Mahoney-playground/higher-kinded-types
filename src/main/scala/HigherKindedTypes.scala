ca17d8be4188a2dd0d10af29acecfe59bd4e70b8
Robert Elliot <rob@lidalia.org.uk>
2015-06-17 20:16:29 +0100

    First cut of convenient client

--- src/main/scala/HigherKindedTypes.scala
+++ src/main/scala/HigherKindedTypes.scala
@@ -19,7 +19,14 @@ class Client2(delegate: Client1) {
   def execute[T](request: Request[T]): T = delegate.execute(request).value
 }
 
+class ConvenientClient(delegate: Client2) {
+  def get[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.GET, classOf[Json]))
+}
+
 object Main extends App {
   val client = new Client2(new Client1)
   val response: Json = client.execute(new Request("http://example.com", Method.GET, classOf[Json]))
+
+  val convenientClient = new ConvenientClient(client)
+  val response2: Json = convenientClient.get("http://example.com", classOf[Json])
 }
