b5708e1030fe267d30c462bfcf7348145a1071d8
Robert Elliot <rob@lidalia.org.uk>
2015-06-17 21:07:11 +0100

    Fixed higher kinded type

--- src/main/scala/HigherKindedTypes.scala
+++ src/main/scala/HigherKindedTypes.scala
@@ -23,7 +23,7 @@ object Client2 {
   type Is[T] = T
 }
 
-class Client2(delegate: Client1) extends BaseClient[Client2.Is] {
+class Client2(delegate: Client1 = new Client1) extends BaseClient[Client2.Is] {
   def execute[T](request: Request[T]): T = delegate.execute(request).value
 }
 
@@ -36,18 +36,15 @@ class Client3 extends BaseClient[Client3.ResponseEitherString] {
 }
 
 class ConvenientClient[Result[_]](delegate: BaseClient[Result]) {
-  def get[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.GET, classOf[Json]))
-  def delete[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.DELETE, classOf[Json]))
+  def get[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.GET, entityType))
+  def delete[T](uri: String, entityType: Class[T]) = delegate.execute(new Request(uri, Method.DELETE, entityType))
 }
 
 object Main extends App {
 
-  val convenientClient1 = new ConvenientClient(new Client1)
-  val response1: Response[Json] = convenientClient1.get("http://example.com", classOf[Json])
+  val response1: Response[Json] = new ConvenientClient(new Client1).get("http://example.com", classOf[Json])
 
-  val convenientClient2 = new ConvenientClient(new Client2(new Client1))
-  val response2: Json = convenientClient2.get("http://example.com", classOf[Json])
+  val response2: String = new ConvenientClient(new Client2).get("http://example.com", classOf[String])
 
-  val convenientClient3 = new ConvenientClient(new Client3)
-  val response3: Response[Either[String, Json]] = convenientClient3.get("http://example.com", classOf[Json])
+  val response3: Response[Either[String, Json]] = new ConvenientClient(new Client3).get("http://example.com", classOf[Json])
 }
